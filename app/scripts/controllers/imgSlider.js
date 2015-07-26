ceac8083b24ff15de9776215b94932e27297aad8
shubham48 <shubhamagarwal48@gmail.com>
2015-07-26 22:57:38 +0530

    image slider

--- app/scripts/controllers/imgSlider.js
+++ app/scripts/controllers/imgSlider.js
@@ -1,35 +1,99 @@
 'use strict';
 
 zopkyFrontendApp.controller('imgSliderController', function ($scope) {
-        $scope.slides = [
-            {image: 'http://farm1.static.flickr.com/260/18577849653_f310e0f6f7_b.jpg', description: 'Image 00'},
-            {image: 'http://farm1.static.flickr.com/494/19453268090_cf9a87188a_b.jpg', description: 'Image 01'},
-            {image: 'http://farm4.static.flickr.com/3765/19348071698_64b2311f9f_b.jpg', description: 'Image 02'},
-            {image: 'http://farm1.static.flickr.com/272/19333133012_3235653356_b.jpg', description: 'Image 03'},
-            {image: 'http://farm1.static.flickr.com/342/19058778119_40bc075676_b.jpg', description: 'Image 04'}
-        ];
 
-        $scope.direction = 'left';
-        $scope.currentIndex = 0;
+        var links = [];
+        $.ajax({
+            url: 'https://api.flickr.com/services/rest/',
+            data: {
+                format: 'json',
+                method: 'flickr.photos.search',
+                api_key: '7617adae70159d09ba78cfec73c13be3',
+                tags: 'Taj Mahal',
+                privacy_filter:1,
+            //    lat: '27.175',
+            //  lon: '78.042',
+            //    radius: '1', 
+            },
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            limit: 20
+        }).done(function (result) {
+            var linksContainer = $('#links'),
+                baseUrl;
+            // Add the demo images as links with thumbnails to the page:
+            $.each(result.photos.photo, function (index, photo) {
+                baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' +
+                    photo.server + '/' + photo.id + '_' + photo.secret;
 
-        $scope.setCurrentSlideIndex = function (index) {
-            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
-            $scope.currentIndex = index;
-        };
+                    var imageDes = 'Image';
+                    if(index<10){
+                        imageDes = imageDes+' 0'+index;
+                    }else{
+                        imageDes=imageDes+' '+index;
+                    }
 
-        $scope.isCurrentSlideIndex = function (index) {
-            return $scope.currentIndex === index;
-        };
+                    var imgObject = {image: baseUrl+ '_b.jpg', description: imageDes};
+                    links.push(imgObject);
 
-        $scope.prevSlide = function () {
+                $('<img>').prop('src', baseUrl + '_s.jpg')
+                    // .append($('<img>').prop('src', baseUrl + '_s.jpg'))
+                    // .prop('href', baseUrl + '_b.jpg')
+                    // .attr('onclick', function(){ $scope.currentIndex = index; console.log("inside onclick"); })
+                    .prop('title', photo.title)
+                    .attr('id', index)
+                    .appendTo(linksContainer);
+            });
+            });
+            $scope.slides=links;
+
+            // $('a.img').click(function() {
+            //     var index = $(this).attr('id');
+            //     console.log(""+index);
+            //     $scope.currentIndex = index;
+            // });
+
+            $('#links').bind('click', function(event) {
+                // alert(event.target.id);
+                var index = event.target.id;
+                console.log("index: "+index);
+                $scope.currentIndex = index;
+                $('.arrow').click();
+             });
+        
+
+        // $scope.slides = [
+        //     {image: 'http://farm1.static.flickr.com/260/18577849653_f310e0f6f7_b.jpg', description: 'Image 00'},
+        //     {image: 'http://farm1.static.flickr.com/494/19453268090_cf9a87188a_b.jpg', description: 'Image 01'},
+        //     {image: 'http://farm4.static.flickr.com/3765/19348071698_64b2311f9f_b.jpg', description: 'Image 02'},
+        //     {image: 'http://farm1.static.flickr.com/272/19333133012_3235653356_b.jpg', description: 'Image 03'},
+        //     {image: 'http://farm1.static.flickr.com/342/19058778119_40bc075676_b.jpg', description: 'Image 04'}
+        // ];
+
+        
+
             $scope.direction = 'left';
-            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-        };
+            $scope.currentIndex = 0;
 
-        $scope.nextSlide = function () {
-            $scope.direction = 'right';
-            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-        };
+            $scope.setCurrentSlideIndex = function (index) {
+                $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+                $scope.currentIndex = index;
+            };
+
+            $scope.isCurrentSlideIndex = function (index) {
+                return $scope.currentIndex === index;
+            };
+
+            $scope.prevSlide = function () {
+                $scope.direction = 'left';
+                $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+            };
+
+            $scope.nextSlide = function () {
+                $scope.direction = 'right';
+                $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+            };
+        
     })
     .animation('.slide-animation', function () {
         return {
@@ -71,33 +135,33 @@ $(function () {
     'use strict';
 
     // Load demo images from flickr:
-    $.ajax({
-        url: 'https://api.flickr.com/services/rest/',
-        data: {
-            format: 'json',
-            method: 'flickr.photos.search',
-            api_key: '7617adae70159d09ba78cfec73c13be3',
-            tags: 'Taj Mahal',
-            privacy_filter:1,
-        //    lat: '27.175',
-        //  lon: '78.042',
-        //    radius: '1', 
-        },
-        dataType: 'jsonp',
-        jsonp: 'jsoncallback'
-    }).done(function (result) {
-        var linksContainer = $('#links'),
-            baseUrl;
-        // Add the demo images as links with thumbnails to the page:
-        $.each(result.photos.photo, function (index, photo) {
-            baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' +
-                photo.server + '/' + photo.id + '_' + photo.secret;
-            $('<a/>')
-                .append($('<img>').prop('src', baseUrl + '_s.jpg'))
-                .prop('href', baseUrl + '_b.jpg')
-                .prop('title', photo.title)
-                .attr('data-gallery', '')
-                .appendTo(linksContainer);
-        });
-    });
+    // $.ajax({
+    //     url: 'https://api.flickr.com/services/rest/',
+    //     data: {
+    //         format: 'json',
+    //         method: 'flickr.photos.search',
+    //         api_key: '7617adae70159d09ba78cfec73c13be3',
+    //         tags: 'Taj Mahal',
+    //         privacy_filter:1,
+    //     //    lat: '27.175',
+    //     //  lon: '78.042',
+    //     //    radius: '1', 
+    //     },
+    //     dataType: 'jsonp',
+    //     jsonp: 'jsoncallback'
+    // }).done(function (result) {
+    //     var linksContainer = $('#links'),
+    //         baseUrl;
+    //     // Add the demo images as links with thumbnails to the page:
+    //     $.each(result.photos.photo, function (index, photo) {
+    //         baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' +
+    //             photo.server + '/' + photo.id + '_' + photo.secret;
+    //         $('<a/>')
+    //             .append($('<img>').prop('src', baseUrl + '_s.jpg'))
+    //             .prop('href', baseUrl + '_b.jpg')
+    //             .prop('title', photo.title)
+    //             .attr('data-gallery', '')
+    //             .appendTo(linksContainer);
+    //     });
+    // });
 });
